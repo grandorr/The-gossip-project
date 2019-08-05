297b60fea9867db53d5d7d883e9382ac4d96743d
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-08-05 23:47:20 +0200

    last

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-User.destroy_all
 Gossip.destroy_all
+User.destroy_all
+
 20.times do
   c = User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, description: Faker::Quote.yoda,email: Faker::Internet.email,age: rand(8..99))
 g = Gossip.create(title: Faker::Book.title,content:  Faker::Quote.yoda, user: c )
